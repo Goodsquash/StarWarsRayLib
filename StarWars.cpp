3707e8d14c0829ff52d00077272e59d44ba6d47c
Goodsquash <conorcrean@hotmail.com>
2022-11-06 14:33:59 +0000

    git commit -a

--- StarWars.cpp
+++ StarWars.cpp
@@ -28,15 +28,17 @@ int main(){
  const int windowHeight{450};
 InitWindow(windowWidth,windowHeight,"My Window");
 
+int framesCounter = 60*10;
+
 Texture2D obstacle = LoadTexture("resources/TieFighter.png");
 Texture2D xwing = LoadTexture("resources/X-Wing.png");
 
+
 float scrollingBack = 0.0f;
-float scrollingMid = 0.0f;
 
 Texture2D midground = LoadTexture("resources/Background.png");
 
-
+//Xwing_animation
 Anim xwingAnim;
 xwingAnim.rec.width = xwing.width/6; 
 xwingAnim.rec.height = xwing.height;
@@ -48,47 +50,43 @@ xwingAnim.frame = 0;
 xwingAnim.updateTime = 1.0/12.0;
 xwingAnim.runningTime = 0.0;
 
+//TieFighter animation
 Anim TieFighter;
 TieFighter.rec.width = obstacle.width;
 TieFighter.rec.height = obstacle.height;
 TieFighter.rec.x=0;
 TieFighter.rec.y=0;
-TieFighter.pos.x = GetRandomValue(500,0);
-TieFighter.pos.y = -100;
+TieFighter.pos.x = GetRandomValue(0,700);
+TieFighter.pos.y = 0;
 
+
+//TieFighter2 animation
 Anim TieFighter2;
 TieFighter2.rec.width = obstacle.width;
 TieFighter2.rec.height = obstacle.height;
 TieFighter2.rec.x=0;
 TieFighter2.rec.y=0;
-TieFighter2.pos.x = GetRandomValue(500,0);
+TieFighter2.pos.x = GetRandomValue(0,700);
 TieFighter2.pos.y = -100;
 
+//TieFighter3 animation
 Anim TieFighter3;
 TieFighter3.rec.width = obstacle.width;
 TieFighter3.rec.height = obstacle.height;
 TieFighter3.rec.x=0;
 TieFighter3.rec.y=0;
-TieFighter3.pos.x = GetRandomValue(500,0);
-TieFighter3.pos.y = -100;
+TieFighter3.pos.x = GetRandomValue(0,700);
+TieFighter3.pos.y = 0;
 
 
-Rectangle obRec;
-obRec.width = obstacle.width;
-obRec.height = obstacle.height; 
-obRec.x = 0;
-obRec.y = 0;
-
-Vector2 obPos; 
-obPos.x = GetRandomValue(0,400);
-obPos.y = GetRandomValue(-100,-50);
-
-
- 
+//Bullet Number
 int num_bullets{50};
 
+//Audio Input Init 
 InitAudioDevice(); 
 
+
+//Player_Class
 Player player;
 player.rec.height= 50;
 player.rec.width = 50;
@@ -100,7 +98,7 @@ player.speed.y = 0;
 
 
 
-
+//Bullets for Xwing
 Bullets bullet[num_bullets];
 for (int i = 0; i<num_bullets; i++){
 bullet[i].rec.height= 10;
@@ -113,34 +111,40 @@ bullet[i].speed.y = -10;
 bullet[i].active = false;}
 int shootRate = 0;
 
-
-int obVel{GetRandomValue(80,300)};
+//TieFighter Speed with random values
 int tieVel{GetRandomValue(80,300)};
+int tieVel2{GetRandomValue(80,300)};
+int tieVel3{GetRandomValue(80,300)};
 
-
-int velocity{0};
-
-
+//X_Wing Movement Speed
 int speed{200};
+
+//Collision varible
 bool collision{};
 
-
+//Music Source
 Music music = LoadMusicStream("resources/Star_Wars_Medley.wav"); 
+
+//Sound Source for Bullet/Lazers
 Sound sound = LoadSound("resources/Quadlaser_turret_fire.wav");
+
+//Play music
 PlayMusicStream(music);
+
+//Collision Death sound
 Sound death= LoadSound("resources/R2D2.wav");
 
-
+//Set FPS
 SetTargetFPS(60);  
 	while(!WindowShouldClose()){
 	
+framesCounter--;
 
-	scrollingBack -= 0.5f;
-    scrollingMid -= 0.5f;
-        
-    
-    if (scrollingMid <= -midground.width*2) scrollingMid = 0;
 
+scrollingBack -= 0.5f; 
+if (scrollingBack <= -midground.width*2) scrollingBack = 0;
+
+//Start music playing
 UpdateMusicStream(music); 
 	
 	const float deltaTime{GetFrameTime()};
@@ -172,31 +176,32 @@ UpdateMusicStream(music);
 
 	if (TieFighter.pos.y > 450) 
 	{
-		TieFighter.pos.x = GetRandomValue(500,0);
-		TieFighter.pos.y = GetRandomValue(-100,0);
+		TieFighter.pos.x = GetRandomValue(0,200);
+		TieFighter.pos.y = 0;
 	} 
 	if (TieFighter2.pos.y > 450) 
 	{
-		TieFighter2.pos.x = GetRandomValue(500,0);
-		TieFighter2.pos.y = GetRandomValue(-100,0);
+		TieFighter2.pos.x = GetRandomValue(210,350);
+		TieFighter2.pos.y = 0;
 	} 
-	if (obPos.y > 450)
+	if (TieFighter3.pos.y > 450) 
 	{
-		obPos.x = GetRandomValue(500,0);
-		obPos.y = GetRandomValue(-100,0);
-	}
+		TieFighter3.pos.x = GetRandomValue(360,450);
+		TieFighter3.pos.y = 0;
+	} 
 
 	if(CheckCollisionRecs(xwingRec,TieFighterRec)){
 		collision = true;
+		
 	}
 	if(CheckCollisionRecs(xwingRec,TieFighterRec2)){
 		collision = true;
+		
 	}
 	if(CheckCollisionRecs(xwingRec,TieFighterRec3)){
 		collision = true;
+		
 	}
-
-
 
 	if(IsKeyDown(KEY_D))
 {
@@ -332,39 +337,59 @@ for (int i = 0; i < num_bullets; i++)
                 bullet[i].active = false;
                 shootRate = 0;
             }
+
         }
     }
 
 
 
 
-ClearBackground(BLACK); 
-	if (collision){
-	DrawText("You Died", 250,200,50,RED);
-	PlaySound(death);
+ClearBackground(BLACK);
+
+if (framesCounter <= 0){
+framesCounter = 0;
+DrawText("You Have Won",150,200,50,BLUE);
 	}
-	
+else
+if (collision){
+DrawText("You Died",250,200,50,RED);
+}
+else
+//Play Death sound
+if (collision){
+PlaySound(death);
+}
+
+
 	else{
 
 
 
 
-DrawTextureEx(midground, (Vector2){ scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
-DrawTextureEx(midground, (Vector2){ midground.width*2 + scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
+
+//Draw Scrolling Back ground
+DrawTextureEx(midground, (Vector2){ scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
+DrawTextureEx(midground, (Vector2){ midground.width*2 + scrollingBack, 20 }, 0.0f, 2.0f,WHITE);
 
 DrawTextureRec(xwing,xwingAnim.rec,xwingAnim.pos,WHITE);
-DrawTextureRec(obstacle,obRec,obPos,WHITE);
-DrawTextureRec(obstacle,TieFighter.rec,TieFighter.pos,WHITE);
 
-xwingAnim.pos.y += velocity * deltaTime; 
-obPos.y += obVel * deltaTime; 
+DrawTextureRec(obstacle,TieFighter.rec,TieFighter.pos,WHITE);
+DrawTextureRec(obstacle,TieFighter2.rec,TieFighter2.pos,WHITE);
+DrawTextureRec(obstacle,TieFighter3.rec,TieFighter3.pos,WHITE);
+
+DrawText(TextFormat("Score: %i", framesCounter/60), 10, 10, 20, LIME);
+
+
 TieFighter.pos.y += tieVel* deltaTime;
+TieFighter2.pos.y += tieVel2* deltaTime;
+TieFighter3.pos.y += tieVel3* deltaTime;
 
 }
 EndDrawing();
 }
 UnloadTexture(xwing);
 UnloadTexture(obstacle);
-UnloadSound(death);
+UnloadTexture(midground);
+
 CloseWindow();
 }
